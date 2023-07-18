eab660ce49707934eb5ee0f6b778c4f95ff72292
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-07-18 20:45:37 +0300

    feat(api): ✨ complete franchise router
    Add getById, Update and Delete functions to Franchise router

--- src/server/api/routers/franchise.ts
+++ src/server/api/routers/franchise.ts
@@ -13,6 +13,53 @@ export const franchiseRouter = createTRPCRouter({
     return res;
   }),
 
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const res = await ctx.prisma.franchise.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!res) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Franchise not found",
+        });
+      }
+      console.log(res);
+      return res;
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const instance = await ctx.prisma.franchise.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!instance) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Franchise not found",
+        });
+      }
+      try {
+        const res = await ctx.prisma.franchise.delete({
+          where: {
+            id: input.id,
+          },
+        });
+        console.log(res);
+        return res;
+      } catch (err) {
+        console.error(err);
+        throw err;
+      }
+    }),
   create: protectedProcedure
     .input(
       z.object({
@@ -22,19 +69,6 @@ export const franchiseRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
-      const user = await ctx.prisma.user.findUnique({
-        where: {
-          id: ctx.session?.user.id,
-        },
-      });
-
-      if (!user) {
-        throw new TRPCError({
-          code: "UNAUTHORIZED",
-          message: "You must be logged in to create a franchise.",
-        });
-      }
-
       const franchise = await ctx.prisma.franchise.create({
         data: {
           name: input.name,
@@ -44,5 +78,41 @@ export const franchiseRouter = createTRPCRouter({
       });
       console.log(franchise);
       return franchise;
+    }),
+
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string().min(1).max(255),
+        description: z.string().min(1).max(255),
+        background_image: z.string().min(1).max(255),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const instance = await ctx.prisma.franchise.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!instance) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Franchise not found",
+        });
+      }
+
+      const res = await ctx.prisma.franchise.update({
+        data: {
+          name: input.name,
+          description: input.description,
+          background_image: input.background_image,
+        },
+        where: {
+          id: input.id,
+        },
+      });
+      return res;
     }),
 });
