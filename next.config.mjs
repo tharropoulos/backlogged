c84e945cbf8810377e9c3a4d7fd29918c791b780
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-07-19 12:40:22 +0300

    build(docker): 🐳 update next config for docker
    Based on https://create.t3.gg/en/deployment/docker#1-next-configuration

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,8 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  swcMinify: true,
+  output: "standalone",
   images: {
     remotePatterns: [
       {
