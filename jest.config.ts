00a765843ef4252727ed758f0746fd6840f56250
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-08-04 22:16:15 +0300

    build: 👷 Update Jest config
    Update jest config to transform tsx files using ts-jest and use the test
    Typescript config when testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,18 @@
 export default {
   clearMocks: true,
-  extensionsToTreatAsEsm: [".ts"],
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
   moduleNameMapper: {
     "^~/(.*)$": "<rootDir>/src/$1",
   },
+  testEnvironment: "node",
   coverageProvider: "v8",
-  preset: "ts-jest/presets/js-with-ts",
+  preset: "ts-jest",
   setupFiles: ["dotenv/config"],
   moduleDirectories: ["node_modules", "src/"],
+  setupFilesAfterEnv: ["<rootDir>/tests/jest-setup.ts"],
   transform: {
-    "^.+\\.mjs$": "ts-jest", // Without this testing breaks
+    "^.+\\.mjs$": "ts-jest",
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "./tsconfig.test.json" }],
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "./tsconfig.test.json" }],
   },
 };
