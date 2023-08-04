a4e63fdda1d610b273af827d9dec16d067a686f0
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-08-04 22:37:24 +0300

    feat(ui): ✨ wrap the app with the theme provider and the SEO component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { api } from "~/utils/api";
+import { api } from "~/lib/api";
 import "~/styles/globals.css";
+import Layout from "./layout";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +11,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 };
