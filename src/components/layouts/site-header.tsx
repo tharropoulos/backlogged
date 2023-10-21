9b08aa821a38caa5bd7edc9023efa49fcfef7a37
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-10-21 20:32:59 +0300

    fix:  bottom margin to site header

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -7,7 +7,7 @@ import { Button } from "../ui/button";
 export function SiteHeader() {
   const { data: sessionData } = useSession();
   return (
-    <header className="sticky top-0 z-40 w-full border-b border-gray-800 bg-background ">
+    <header className="sticky top-0 z-40 mb-7 w-full border-b border-gray-800 bg-background ">
       <div className="container flex h-16 items-center justify-between px-6 ">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex items-center gap-3">
