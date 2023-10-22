24fcfddfaddf7b6a8ac925b972f241bbfd343ff9
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-10-22 19:43:12 +0300

    feat: extend publisher schema zod validator

--- src/lib/validations/publisher.ts
+++ src/lib/validations/publisher.ts
@@ -9,3 +9,22 @@ export const createPublisherSchema = z.object({
     .max(255),
   coverImage: z.string().min(1).max(255),
 });
+
+export const publisherSchema = createPublisherSchema.extend({
+  id: z.string().cuid(),
+  // NOTE: Copilot generated this, with no input from me
+  games: z
+    .array(
+      z.object({
+        id: z.string().cuid(),
+        name: z.string().min(1).max(255),
+        description: z.string().min(1).max(255),
+        releaseDate: z.date(),
+        franchiseId: z.string().cuid(),
+        publisherId: z.string().cuid(),
+        coverImage: z.string().min(1).max(255),
+        backgroundImage: z.string().min(1).max(255),
+      })
+    )
+    .optional(),
+});
