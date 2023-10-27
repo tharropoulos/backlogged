416dff99f276847c854ad0542087e4de1b2cead7
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-10-27 18:50:06 +0300

    feat: genre zod validation

---
+++ src/lib/validations/genre.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+import { gameSchema } from "./game";
+
+export const createGenreSchema = z.object({
+  name: z.string().min(1, { message: "A name is required" }).max(191),
+  description: z
+    .string()
+    .min(1, { message: "A description is required" })
+    .max(191),
+});
+
+export const genreSchema = createGenreSchema.extend({
+  id: z.string().cuid(),
+  games: z.array(gameSchema).optional(),
+});
