08fde1df6954a473e86e8fd47f26dbf5eb385441
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-10-27 18:50:48 +0300

    fix(feature): use gameSchema instead of new zod object on zod validation

--- src/lib/validations/feature.ts
+++ src/lib/validations/feature.ts
@@ -1,5 +1,6 @@
 //NOTE: Written by Copilot Chat
 import { z } from "zod";
+import { gameSchema } from "./game";
 
 export const createFeatureSchema = z.object({
   name: z.string().min(1, { message: "A name is required" }).max(255),
@@ -11,18 +12,5 @@ export const createFeatureSchema = z.object({
 
 export const featureSchema = createFeatureSchema.extend({
   id: z.string().cuid2(),
-  games: z
-    .array(
-      z.object({
-        id: z.string().cuid2(),
-        name: z.string().min(1).max(255),
-        description: z.string().min(1).max(255),
-        releaseDate: z.date(),
-        franchiseId: z.string().cuid(),
-        publisherId: z.string().cuid(),
-        coverImage: z.string().min(1).max(255),
-        backgroundImage: z.string().min(1).max(255),
-      })
-    )
-    .optional(),
+  games: z.array(gameSchema).optional(),
 });
