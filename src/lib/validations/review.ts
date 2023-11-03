ba1ac36a7a0d93e938809fc616bc50a4e37be1d1
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-11-03 19:48:28 +0200

    feat: review zod validation

---
+++ src/lib/validations/review.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+//NOTE: Written by Copilot
+export const createReviewSchema = z.object({
+  gameId: z.string().cuid2(),
+  rating: z.number().min(1).max(5).nullable(),
+  content: z.string().min(1).max(5000),
+  userId: z.string().cuid2(),
+});
+
+export const reviewSchema = createReviewSchema.extend({
+  id: z.string().cuid2(),
+  likes: z
+    .array(
+      z.object({
+        userId: z.string().cuid2(),
+      })
+    )
+    .nullish(),
+  comments: z
+    .array(
+      z.object({
+        userId: z.string().cuid2(),
+        content: z.string().min(1).max(5000),
+      })
+    )
+    .nullish(),
+  // likes: z.object({
+  //   userId: z.string().cuid2(),
+  // }),
+});
