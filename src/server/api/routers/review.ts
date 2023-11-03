01fb4819b7b213f7a4114d5dc87d415b16eb208d
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-11-03 20:16:22 +0200

    fix: third iteration of review router
    GENERATED ENTIRELY BY COPILOT

--- src/server/api/routers/review.ts
+++ src/server/api/routers/review.ts
@@ -5,73 +5,103 @@ import {
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-
+import { createReviewSchema } from "~/lib/validations/review";
 //NOTE: Written by Copilot
 
 export const reviewRouter = createTRPCRouter({
   create: protectedProcedure
-    .input(
-      z.object({
-        gameId: z.string(),
-        userId: z.string(),
-        rating: z.number().optional(),
-        content: z.string(),
-      })
-    )
+    .input(createReviewSchema)
     .mutation(async ({ ctx, input }) => {
-      const res = await ctx.prisma.review.create({
-        data: input,
-      });
-      return res;
+      try {
+        const res = await ctx.prisma.review.create({
+          data: {
+            ...input,
+            game: {
+              connect: { id: input.gameId },
+            },
+            user: {
+              connect: { id: input.userId },
+            },
+          },
+        });
+        return res;
+      } catch (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: error.message,
+        });
+      }
     }),
 
   getById: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input }) => {
-      const res = await ctx.prisma.review.findUnique({
-        where: {
-          id: input.id,
-        },
-      });
+      try {
+        const res = await ctx.prisma.review.findUnique({
+          where: {
+            id: input.id,
+          },
+        });
 
-      if (!res) {
+        if (!res) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Review not found",
+          });
+        }
+
+        return res;
+      } catch (error) {
         throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "Review not found",
+          code: "INTERNAL_SERVER_ERROR",
+          message: error.message,
         });
       }
-
-      return res;
     }),
 
   getAll: publicProcedure.query(async ({ ctx }) => {
-    const res = await ctx.prisma.review.findMany();
-    return res;
+    try {
+      const res = await ctx.prisma.review.findMany();
+      return res;
+    } catch (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: error.message,
+      });
+    }
   }),
 
   update: protectedProcedure
-    .input(
-      z.object({
-        id: z.string(),
-        rating: z.number().optional(),
-        content: z.string().optional(),
-      })
-    )
+    .input(createReviewSchema.extend({ id: z.string() }))
     .mutation(async ({ ctx, input }) => {
-      const res = await ctx.prisma.review.update({
-        where: { id: input.id },
-        data: input,
-      });
-      return res;
+      try {
+        const res = await ctx.prisma.review.update({
+          where: { id: input.id },
+          data: input,
+        });
+        return res;
+      } catch (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: error.message,
+        });
+      }
     }),
 
   delete: protectedProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ ctx, input }) => {
-      const res = await ctx.prisma.review.delete({
-        where: { id: input.id },
-      });
-      return res;
+      try {
+        const res = await ctx.prisma.review.delete({
+          where: { id: input.id },
+        });
+        return res;
+      } catch (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: error.message,
+        });
+      }
     }),
 
   like: protectedProcedure
@@ -82,18 +112,39 @@ export const reviewRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
-      const res = await ctx.prisma.reviewLike.create({
-        data: input,
-      });
-      return res;
+      try {
+        const res = await ctx.prisma.reviewLike.create({
+          data: {
+            review: {
+              connect: { id: input.reviewId },
+            },
+            user: {
+              connect: { id: input.userId },
+            },
+          },
+        });
+        return res;
+      } catch (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: error.message,
+        });
+      }
     }),
 
   unlike: protectedProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ ctx, input }) => {
-      const res = await ctx.prisma.reviewLike.delete({
-        where: { id: input.id },
-      });
-      return res;
+      try {
+        const res = await ctx.prisma.reviewLike.delete({
+          where: { id: input.id },
+        });
+        return res;
+      } catch (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: error.message,
+        });
+      }
     }),
 });
