a6e5eb43a118bd379c109a8e37e5600e58758a82
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-11-13 18:42:09 +0200

    fix: include id in commment details result

--- src/server/api/routers/comment.ts
+++ src/server/api/routers/comment.ts
@@ -197,6 +197,7 @@ export const commentRouter = createTRPCRouter({
               likes: true,
             },
           },
+          id: true,
           content: true,
           user: {
             select: {
