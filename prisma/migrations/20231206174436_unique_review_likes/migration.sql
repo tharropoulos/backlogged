468c545d5422a20eba849d384a78ae6bf7cbc154
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-12-29 22:24:17 +0200

    fix(db): use prisma db push instead of migrations

--- prisma/migrations/20231206174436_unique_review_likes/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[reviewId,userId]` on the table `ReviewLike` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `ReviewLike_reviewId_userId_key` ON `ReviewLike`(`reviewId`, `userId`);
--- prisma/migrations/20231207122043_remove_deleted_from_comment_likes/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `deleted` on the `CommentLike` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `CommentLike` DROP COLUMN `deleted`;
